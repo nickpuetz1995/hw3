eb5c3baf12e30c403ea88e3ba4db23ea3551406c
nickpuetz1995 <nick.puetz@kellogg.northwestern.edu>
2022-05-18 03:47:18 +0000

    misc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
         @post["posted_on"] = params["post"]["posted_on"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/places/#{@post[place_id]}"
+        redirect_to "/places/#{@post["place_id"]}"
     end
 
 end
