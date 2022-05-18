64729af608c8eed1cd4ce2e75f2993644a6e1f1d
nickpuetz1995 <nick.puetz@kellogg.northwestern.edu>
2022-05-18 02:41:35 +0000

    update relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new
-        @post
+        @post["title"] = params["post]["title"]
+        @post["description"] = params["post]["description"]
+        @post["posted_on"] = params["post]["posted_on"]
+        @post["place_id"] = params["post]["place_id"]
+        @post.save
+        redirect_to "/places/#{@post[place_id]}"
 
 end
